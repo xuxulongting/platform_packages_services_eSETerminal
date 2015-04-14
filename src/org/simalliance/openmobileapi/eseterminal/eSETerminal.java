84f63c5314dd534bb770bf7b59abba896eb10597
Sergi Sevil <sergi.sevil@gi-de.com>
2015-04-14 13:11:09 +0200

    Set bind permissions of the service at manifest

--- src/org/simalliance/openmobileapi/eseterminal/eSETerminal.java
+++ src/org/simalliance/openmobileapi/eseterminal/eSETerminal.java
@@ -44,21 +44,8 @@ public final class eSETerminal extends Service {
     private boolean mNFCAdapaterOpennedSuccesful = false;
 
     @Override
-    public IBinder onBind(Intent intent) {
-        String packageName = intent.getPackage();
-        try {
-            Log.d(TAG, "Package NAME: " + packageName);
-            String[] permissions = getPackageManager().getPackageInfo(packageName, PackageManager.GET_PERMISSIONS).requestedPermissions;
-            for (String permission : permissions) {
-                if ("org.simalliance.openmobileapi.BIND_TERMINAL".equals(permission)) {
-                    return mTerminalBinder;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        Log.d(TAG, "Not allowed to bind");
-        return null;
+    public IBinder onBind(Intent intent) throws SecurityException {
+        return mTerminalBinder;
     }
 
     @Override
